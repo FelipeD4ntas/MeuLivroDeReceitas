ab7957e29795f9c28d91f3cfd6270c040fff8526
MarcosD4ntas <felipeicedantas@gmail.com>
2023-04-05 14:56:40 -0300

    classes para acessar o repositorio

--- src/Backend/MeuLivroDeReceitas.Infra/Bootstrapper.cs
+++ src/Backend/MeuLivroDeReceitas.Infra/Bootstrapper.cs
@@ -1,8 +1,12 @@
 ﻿using FluentMigrator.Runner;
 using MeuLivroDeReceitas.Domain.Extension;
+using MeuLivroDeReceitas.Domain.Repositorios;
+using MeuLivroDeReceitas.Infra.AcessoRepositorio;
+using MeuLivroDeReceitas.Infra.AcessoRepositorio.Repositorios;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
+using Microsoft.EntityFrameworkCore;
 
 namespace MeuLivroDeReceitas.Infra;
 
@@ -11,6 +15,10 @@ public static class Bootstrapper
     public static void AddRepositorio(this IServiceCollection services, IConfiguration configuration)
     {
         AddFluentMigrator(services, configuration);
+
+        AddRepositorios(services);
+        AddContext(services, configuration);
+        AddUnidadeDeTrabalho(services);
     }
 
     private static void AddFluentMigrator(IServiceCollection services, IConfiguration configuration)
@@ -21,5 +29,28 @@ public static class Bootstrapper
             c.AddMySql5()
                 .WithGlobalConnectionString(connectionStringCompleta)
                 .ScanIn(Assembly.Load("MeuLivroDeReceitas.Infra")).For.All());
+    }
+
+    private static void AddContext(IServiceCollection services, IConfiguration configuration)
+    {
+        var versaoServidor = new MySqlServerVersion(new Version(8, 0, 32));
+        var connectionString = configuration.GetConnectionString("Conexao");
+
+        services.AddDbContext<MeuLivroDeReceitasContext>(options =>
+        {
+            options.UseMySql(connectionString, versaoServidor);
+        });
+    }
+
+    private static void AddUnidadeDeTrabalho(IServiceCollection services)
+    {
+        services.AddScoped<IUnidadeDeTrabalho, UnidadeDeTrabalho>();
+    }
+
+    private static void AddRepositorios(IServiceCollection services)
+    {
+        services.AddScoped<IUsuarioWriteOnlyRepositorio, UsuarioRepositorio>();
+        services.AddScoped<IUsuarioReadOnlyRepositorio, UsuarioRepositorio>();
+
     }
 }
